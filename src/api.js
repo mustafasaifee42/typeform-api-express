0d20cf020c1d5e2874ce24fc8f326c531aecf714
mustafasaifee42 <saifee.mustafa@gmail.com>
2019-11-21 00:00:52 +0200

    adding typeform

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
+var cors = require('cors');
+var axios = require('axios');
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+router.get('/', function(req, res, next) {
+  axios.get('https://api.typeform.com/forms/eaj1sU/responses?completed=true&page_size=1000',{ headers: { Authorization: 'Bearer Hr3EEcXr52zkTRHrS4N2mTRk4SAZLAd3VwUnsoNbDCK9', "Access-Control-Allow-Origin": "*"} }).then(d => {
+      console.log(d)
+      res.json(d.data);
+  })
 });
 
 app.use(`/.netlify/functions/api`, router);
